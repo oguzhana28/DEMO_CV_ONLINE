83ecf1505cfd467d6867cd42cbb36990fd3a4d1f
oguzhanarslan <oguzhan28@hotmail.nl>
2020-02-09 18:46:44 +0100

    My first commit of cv online

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ URL::asset('css/stylesheet.css') }}" rel="stylesheet">
+
+        <title>@yield('title')</title>
+    </head>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('includes.header')
+
+@section('title', 'Welcome')
+
+<body>
+    <h1 class="title">CSV DEMO Oguzhan<h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+
+@extends('includes.footer')
+
